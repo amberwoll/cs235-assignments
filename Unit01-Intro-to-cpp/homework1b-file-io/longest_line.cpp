4a01c971dd9f6cf58026e400f13507813e817df9
Amber <woll.ambermadison@gmail.com>
2023-09-20 21:19:55 -0600

    homework 1b

--- Unit01-Intro-to-cpp/homework1b-file-io/longest_line.cpp
+++ Unit01-Intro-to-cpp/homework1b-file-io/longest_line.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
 
 int main(int argc, char const* argv[]) {
-    // Write your code here
+    ifstream inputFile(argv[1]);
+
+    string longestLine;
+    string line;
+    string word;
+    int wordCount = 0;
+    int longestLineCount = 0;
+
+    while (getline(inputFile, line)) {
+        stringstream ss(line);
+        while (ss >> word) {
+            wordCount++;
+        }
+        if (wordCount > longestLineCount) {
+            longestLine = line;
+            longestLineCount = wordCount;
+        };
+        wordCount = 0;
+    };
+
+    cout << longestLine << endl;
 
     return 0;
 }
--- Unit01-Intro-to-cpp/homework1b-file-io/reformat.cpp
+++ Unit01-Intro-to-cpp/homework1b-file-io/reformat.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+
+using namespace std;
 
 int main(int argc, char const* argv[]) {
-    // Write your code here
+    ifstream inputFile(argv[1]);
+    ofstream outputFile(argv[2]);
+
+    string first;
+    string last;
+    int points;
+    double factor;
+
+    while (inputFile >> first >> last >> points >> factor) {
+        double newPoints = factor * points;
+        outputFile << last << ", " << first << ": " << newPoints << endl;
+    }
 
     return 0;
 }
