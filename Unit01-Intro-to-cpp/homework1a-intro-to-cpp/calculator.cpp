b3b59d71c1270a8cd5fd7ca28f3aa28dd67a80ea
Amber <woll.ambermadison@gmail.com>
2023-09-20 20:06:10 -0600

    lab 1a

--- Unit01-Intro-to-cpp/homework1a-intro-to-cpp/calculator.cpp
+++ Unit01-Intro-to-cpp/homework1a-intro-to-cpp/calculator.cpp
@@ -10,7 +10,41 @@ std::string input(std::string prompt) {
 }
 
 int main(int argc, char const* argv[]) {
-    // Write your code here
+    while (true) {
+        std::string operation = input("operation: ");
 
+        if (operation.empty()) {
+            break;
+        }
+
+        double left_operand, right_operand, result;
+
+        left_operand = std::stoi(input("left operand: "));
+        right_operand = std::stoi(input("right operand: "));
+
+        if (operation == "add") {
+            result = left_operand + right_operand;
+
+        } else if (operation == "subtract") {
+            result = left_operand - right_operand;
+
+        } else if (operation == "multiply") {
+            result = left_operand * right_operand;
+
+        } else if (operation == "divide") {
+            result = left_operand / right_operand;
+
+        } else if (operation == "mod") {
+            int left_int = static_cast<int>(left_operand);
+            int right_int = static_cast<int>(right_operand);
+            result = left_int % right_int;
+
+        } else {
+            std::cout << operation << " isn't a valid operation" << std::endl;
+            break;
+        }
+
+        std::cout << result << std::endl;
+    }
     return 0;
 }
--- Unit01-Intro-to-cpp/homework1a-intro-to-cpp/numbers_divisible_by.cpp
+++ Unit01-Intro-to-cpp/homework1a-intro-to-cpp/numbers_divisible_by.cpp
@@ -2,7 +2,23 @@
 #include <string>
 
 int main(int argc, char const* argv[]) {
-    // Write your code here
+    int start = std::stoi(argv[1]);
+    int end = std::stoi(argv[2]);
+    int divisor = std::stoi(argv[3]);
+
+    if (start <= end) {
+        for (int i = start; i <= end; i++) {
+            if (i % divisor == 0) {
+                std::cout << i << std::endl;
+            }
+        }
+    } else {
+        for (int i = start; i >= end; i--) {
+            if (i % divisor == 0) {
+                std::cout << i << std::endl;
+            }
+        }
+    }
 
     return 0;
 }
