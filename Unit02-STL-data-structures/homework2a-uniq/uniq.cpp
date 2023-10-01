662a1ceb3c0cd38b8d89aa09e2eef1aae4538f66
Amber <woll.ambermadison@gmail.com>
2023-09-30 21:53:47 -0600

    unit 2

--- Unit02-STL-data-structures/homework2a-uniq/uniq.cpp
+++ Unit02-STL-data-structures/homework2a-uniq/uniq.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <unordered_set>
 
 int main(int argc, char const* argv[]) {
-    // Write your code here
+    std::unordered_set<std::string> uniq;
+    std::string new_line;
 
+    if (argv[1]) {
+        std::ifstream inputFile(argv[1]);
+        while (std::getline(inputFile, new_line)) {
+            if (uniq.find(new_line) == uniq.end()) { 
+                uniq.insert(new_line);
+                std::cout << new_line << std::endl;
+            };
+        };
+    }
+    else {
+        while (std::getline(std::cin, new_line)) {
+            if (uniq.find(new_line) == uniq.end()) {
+                uniq.insert(new_line);
+                std::cout << new_line << std::endl;
+            };
+        };
+    };
     return 0;
 }
