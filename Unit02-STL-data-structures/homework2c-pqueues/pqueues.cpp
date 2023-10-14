90850f7e75eb2038fe501c4348681eea50d86db4
Amber <woll.ambermadison@gmail.com>
2023-10-13 18:50:59 -0600

    final lab 2c

--- Unit02-STL-data-structures/homework2c-pqueues/pqueues.cpp
+++ Unit02-STL-data-structures/homework2c-pqueues/pqueues.cpp
@@ -10,31 +10,35 @@ int main(int argc, char const* argv[]) {
     string operation;
     cout << "What do you want to do? ";
     getline(cin, operation);
-    if (operation == "add") {
-        string name;
-        cout << "Name: ";
-        getline(cin, name);
-        string priority;
-        cout << "Priority: ";
-        getline(cin, priority);
-        string stringPriority = (stoi(priority) < 10) ? "0" + priority : priority;
-        string entry = stringPriority + " - " + name;
-        PQ.push(entry);
-    }
-    else if (operation == "take") {
-        if (!PQ.empty()) {
-            string next = PQ.top();
-            PQ.pop();
-            cout << next << endl;
-        } else {
-            cout << "There are no more people in line" << endl;
+    while (true) {
+        if (operation == "add") {
+            string name;
+            cout << "Name: ";
+            getline(cin, name);
+            string priority;
+            cout << "Priority: ";
+            getline(cin, priority);
+            string stringPriority = (stoi(priority) < 10) ? "0" + priority : priority;
+            string entry = stringPriority + " - " + name;
+            PQ.push(entry);
         }
-    }
-    else if (operation == "") {
-        return 0;
-    }
-    else {
-        cout << operation << " isn't a valid operation" << endl;
+        else if (operation == "take") {
+            if (!PQ.empty()) {
+                string next = PQ.top();
+                PQ.pop();
+                cout << next << endl;
+            } else {
+                cout << "There are no more people in line" << endl;
+            }
+        }
+        else if (operation == "") {
+            return 0;
+        }
+        else {
+            cout << operation << " isn't a valid operation" << endl;
+        }
+        cout << "What do you want to do? ";
+        getline(cin, operation);
     }
     return 0;
 }
