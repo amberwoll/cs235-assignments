d471d305a571096dce879ebfea37f69953b2663c
Amber <woll.ambermadison@gmail.com>
2023-10-26 18:40:12 -0600

    Update maze.cpp

--- Unit03-Sorting/homework3b-maze/maze.cpp
+++ Unit03-Sorting/homework3b-maze/maze.cpp
@@ -6,5 +6,21 @@
 int main(int argc, char* argv[]) {
     // Write your code here
 
-    return 0;
+if (maze[r][c] == 0)
+        return false;
+    
+    if ((r == 3) && (c == 3) && (maze[r][c] == 1))
+        return true;
+    
+    if ((r < 0) || (c < 0) || (r >= 4) || (r >= 4))
+        return false;
+
+    //if we have been there before base case
+
+    if (solveMaze(maze, r-1, c) ||
+        solveMaze(maze, r+1, c) ||
+        solveMaze[r][c+1] || 
+        solveMaze[r][c-1])
+
+    return false;
 }
