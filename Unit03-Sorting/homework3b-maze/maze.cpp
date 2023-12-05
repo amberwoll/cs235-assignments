6e36a11ef9b6aa11f9f9bd20e1c41815beaff3ed
Amber <woll.ambermadison@gmail.com>
2023-12-04 22:39:10 -0700

    homework 3b part 2

--- Unit03-Sorting/homework3b-maze/maze.cpp
+++ Unit03-Sorting/homework3b-maze/maze.cpp
@@ -4,36 +4,38 @@
 
 #include "Grid.h"
 
-bool solveMaze(Grid& maze, std::vector<std::vector<std::vector<bool>>>& visited, int r, int c, int l) {
-
+bool solveMaze(Grid& maze, Grid& visited, int r, int c, int l) {
     if (r < 0 || c < 0 || l < 0 || r >= maze.height() || c >= maze.width() || l >= maze.depth() || maze.at(r, c, l) == 0)
         return false;
 
-    if (r == maze.height() - 1 && c == maze.width() - 1 && l == maze.depth() - 1)
+    if ((r == maze.height() - 1 && c == maze.width() - 1 && l == maze.depth() - 1) && (maze.at(r, c, l) == 1))
         return true;
 
-    visited[r][c][l] = true;
+    visited.at(r, c, l) = true;
 
     if (solveMaze(maze, visited, r - 1, c, l) || solveMaze(maze, visited, r + 1, c, l) ||
         solveMaze(maze, visited, r, c - 1, l) || solveMaze(maze, visited, r, c + 1, l) ||
         solveMaze(maze, visited, r, c, l - 1) || solveMaze(maze, visited, r, c, l + 1)) {
         return true;
     }
-
-    visited[r][c][l] = false;
     return false;
 }
 
 int main(int argc, char* argv[]) {
-
     std::ifstream inFile(argv[1]);
     std::ofstream outFile(argv[2]);
     Grid maze;
     inFile >> maze;
 
-    std::vector<std::vector<std::vector<bool>>> visited(maze.height(),
-                                                        std::vector<std::vector<bool>>(maze.width(),
-                                                                                       std::vector<bool>(maze.depth(), false)));
+    Grid visited;
+    visited = maze;
+    for (int r = 0; r < visited.height(); ++r) {
+        for (int c = 0; c < visited.width(); ++c) {
+            for (int l = 0; l < visited.depth(); ++l) {
+                visited.at(r, c, l) = false;
+            }
+        }
+    }
 
     if (solveMaze(maze, visited, 0, 0, 0)) {
         outFile << "SOLUTION\n";
@@ -41,26 +43,21 @@ int main(int argc, char* argv[]) {
         int currentRow = 0;
         int currentColumn = 0;
 
-        std::vector<std::vector<std::vector<bool>>> visited(maze.height(),
-                                                        std::vector<std::vector<bool>>(maze.width(),
-                                                                                       std::vector<bool>(maze.depth(), false)));
-
-
         while (!(currentRow == maze.height() - 1 && currentColumn == maze.width() - 1 && currentLevel == maze.depth() - 1)) {
             outFile << currentRow << " " << currentColumn << " " << currentLevel << '\n';
-            visited[currentRow][currentColumn][currentLevel] = true;
+            visited.at(currentRow, currentColumn, currentLevel) = true;
 
-            if (currentRow - 1 >= 0 && maze.at(currentRow - 1, currentColumn, currentLevel) == 1 && !visited[currentRow - 1][currentColumn][currentLevel]) {
+            if (currentRow - 1 >= 0 && maze.at(currentRow - 1, currentColumn, currentLevel) == 1 && !visited.at(currentRow - 1, currentColumn, currentLevel)) {
                 currentRow--;
-            } else if (currentRow + 1 < maze.height() && maze.at(currentRow + 1, currentColumn, currentLevel) == 1 && !visited[currentRow + 1][currentColumn][currentLevel]) {
+            } else if (currentRow + 1 < maze.height() && maze.at(currentRow + 1, currentColumn, currentLevel) == 1 && !visited.at(currentRow + 1, currentColumn, currentLevel)) {
                 currentRow++;
-            } else if (currentColumn - 1 >= 0 && maze.at(currentRow, currentColumn - 1, currentLevel) == 1 && !visited[currentRow][currentColumn - 1][currentLevel]) {
+            } else if (currentColumn - 1 >= 0 && maze.at(currentRow, currentColumn - 1, currentLevel) == 1 && !visited.at(currentRow, currentColumn - 1, currentLevel)) {
                 currentColumn--;
-            } else if (currentColumn + 1 < maze.width() && maze.at(currentRow, currentColumn + 1, currentLevel) == 1 && !visited[currentRow][currentColumn + 1][currentLevel]) {
+            } else if (currentColumn + 1 < maze.width() && maze.at(currentRow, currentColumn + 1, currentLevel) == 1 && !visited.at(currentRow, currentColumn + 1, currentLevel)) {
                 currentColumn++;
-            } else if (currentLevel - 1 >= 0 && maze.at(currentRow, currentColumn, currentLevel - 1) == 1 && !visited[currentRow][currentColumn][currentLevel - 1]) {
+            } else if (currentLevel - 1 >= 0 && maze.at(currentRow, currentColumn, currentLevel - 1) == 1 && !visited.at(currentRow, currentColumn, currentLevel - 1)) {
                 currentLevel--;
-            } else if (currentLevel + 1 < maze.depth() && maze.at(currentRow, currentColumn, currentLevel + 1) == 1 && !visited[currentRow][currentColumn][currentLevel + 1]) {
+            } else if (currentLevel + 1 < maze.depth() && maze.at(currentRow, currentColumn, currentLevel + 1) == 1 && !visited.at(currentRow, currentColumn, currentLevel + 1)) {
                 currentLevel++;
             }
         }
@@ -72,5 +69,4 @@ int main(int argc, char* argv[]) {
     inFile.close();
     outFile.close();
     return 0;
-
 }
