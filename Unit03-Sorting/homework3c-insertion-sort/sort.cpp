1630bd26c297f06c9ec6d708fc4d7b773b376d0d
Amber <woll.ambermadison@gmail.com>
2023-12-04 23:02:50 -0700

    homework 3c

--- Unit03-Sorting/homework3c-insertion-sort/sort.cpp
+++ Unit03-Sorting/homework3c-insertion-sort/sort.cpp
@@ -1,8 +1,41 @@
 #include <iostream>
+#include <fstream>
 #include <string>
+#include <vector>
+
+using namespace std;
+
+void insertSort(vector<string> &arrange) {
+    int n = arrange.size();
+    for (int i = 1; i < n; ++i) {
+        string save = arrange[i];
+        int j = i - 1;
+        while (j >= 0 && arrange[j] > save) {
+            arrange[j + 1] = arrange[j];
+            j = j - 1;
+        }
+        arrange[j + 1] = save;
+    }
+}
 
 int main(int argc, char const* argv[]) {
-    cin argv[0]
+    vector<string> lines; 
+    ifstream inputFile(argv[1]);
+
+    string line;
+    while (getline(inputFile, line)) {
+        if (line.empty()) {
+            continue;
+        }
+        lines.push_back(line);
+    }
+    inputFile.close();
+
+    insertSort(lines);
+
+    for (const auto &line : lines) {
+        cout << line << endl;
+    }
 
     return 0;
 }
